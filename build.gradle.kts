df0d4728c7941573cd0a86168573dd0c34323e4c
Wzp-2008 <minecraftwzpmc@163.com>
2024-12-11 12:27:49 +0800

    initialization commit

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("java-library")
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "org.mmga"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    maven {
+        url = uri("https://maven.aliyun.com/repository/public/")
+    }
+    maven {
+        url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+    }
+    mavenCentral()
+}
+
+dependencies {
+    api("org.springframework.boot:spring-boot-starter-actuator")
+    api("org.springframework.boot:spring-boot-starter-web")
+    api("org.springframework.boot:spring-boot-starter-websocket")
+    api("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.3")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.mybatis.spring.boot:mybatis-spring-boot-starter-test:3.0.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/org/mmga/spring/boot/starter/MakeMinecraftGreatAgainSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package org.mmga.spring.boot.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMinecraftGreatAgainSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeMinecraftGreatAgainSpringBootStarterApplication.class, args);
+    }
+
+}
