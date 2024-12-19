a8abe114342b2efcdc1d613ab5416f5ab8927e43
Wzp-2008 <minecraftwzpmc@163.com>
2024-12-19 19:09:18 +0800

    feat: add constructor to PagerData

--- src/main/java/org/mmga/spring/boot/starter/entities/PagerData.java
+++ src/main/java/org/mmga/spring/boot/starter/entities/PagerData.java
@@ -1,12 +1,14 @@
 package org.mmga.spring.boot.starter.entities;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 @Schema(name = "分页数据")
+@AllArgsConstructor
 public class PagerData<T> {
     @Schema(description = "总数据量")
     private long total;
